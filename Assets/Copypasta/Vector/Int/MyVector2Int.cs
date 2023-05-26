b880f9f89beeab9941ed39456e2e9e9f7186b874
habrador <erik.nordeus@gmail.com>
2023-05-26 13:11:51 +0200

    Added more vector operations to the int vectors

--- Assets/Copypasta/Vector/Int/MyVector2Int.cs
+++ Assets/Copypasta/Vector/Int/MyVector2Int.cs
@@ -13,5 +13,60 @@ namespace Copypasta
             this.x = x;
             this.y = y;
         }
+
+
+
+        //
+        // Generate new vectors
+        //
+
+        public static MyVector2Int Zero => new(0, 0);
+        public static MyVector2Int One => new(1, 1);
+
+
+
+        //Returns true if the given vector is exactly equal to this vector
+        public bool Equals(MyVector2Int other)
+        {
+            if (this.x == other.x && this.y == other.y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        //
+        // Operator overloads
+        //
+
+        public static MyVector2Int operator *(MyVector2Int vec, int a)
+        {
+            return new(vec.x * a, vec.y * a);
+        }
+
+        public static MyVector2Int operator *(int a, MyVector2Int vec)
+        {
+            return new(vec.x * a, vec.y * a);
+        }
+
+        public static MyVector2Int operator +(MyVector2Int vecA, MyVector2Int vecB)
+        {
+            return new(vecA.x + vecB.x, vecA.y + vecB.y);
+        }
+
+        public static MyVector2Int operator -(MyVector2Int vecA, MyVector2Int vecB)
+        {
+            return new(vecA.x - vecB.x, vecA.y - vecB.y);
+        }
+
+        public static MyVector2Int operator -(MyVector2Int a)
+        {
+            return a * -1;
+        }
     }
 }
--- Assets/Copypasta/Vector/Int/MyVector3Int.cs
+++ Assets/Copypasta/Vector/Int/MyVector3Int.cs
@@ -14,5 +14,61 @@ namespace Copypasta
             this.y = y;
             this.z = z;
         }
+
+
+
+        //
+        // Generate new vectors
+        //
+
+        public static MyVector3Int Zero => new(0, 0, 0);
+        public static MyVector3Int One => new(1, 1, 1);
+
+
+
+        //Returns true if the given vector is exactly equal to this vector
+        public bool Equals(MyVector3Int other)
+        {
+            if (this.x == other.x && this.y == other.y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+
+        //
+        // Operator overloads
+        //
+
+        public static MyVector3Int operator *(MyVector3Int vec, int a)
+        {
+            return new(vec.x * a, vec.y * a, vec.z * a);
+        }
+
+        public static MyVector3Int operator *(int a, MyVector3Int vec)
+        {
+            return new(vec.x * a, vec.y * a, vec.z * a);
+        }
+
+        public static MyVector3Int operator +(MyVector3Int vecA, MyVector3Int vecB)
+        {
+            return new(vecA.x + vecB.x, vecA.y + vecB.y, vecA.z + vecB.z);
+        }
+
+        public static MyVector3Int operator -(MyVector3Int vecA, MyVector3Int vecB)
+        {
+            return new(vecA.x - vecB.x, vecA.y - vecB.y, vecA.z - vecB.z);
+        }
+
+        public static MyVector3Int operator -(MyVector3Int a)
+        {
+            return a * -1;
+        }
     }
 }
