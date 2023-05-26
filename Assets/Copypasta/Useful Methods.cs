a02cfb404276fff16b8866ca82fc10f4c1b51e59
habrador <erik.nordeus@gmail.com>
2023-05-26 11:16:55 +0200

    Added todo list

--- Assets/Copypasta/Useful Methods.cs
+++ Assets/Copypasta/Useful Methods.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UsefulMethods : MonoBehaviour
+namespace Copypasta
 {
+    public class UsefulMethods
+    {
     
+    }
 }
