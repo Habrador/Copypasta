ce4394e087d37d9f3c69802f850df6feeee1648f
habrador <erik.nordeus@gmail.com>
2023-05-26 12:32:15 +0200

    Added custom vector2 and vector3 int libraries

---
+++ Assets/Copypasta/Vector/Int/MyVector2Int.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Copypasta
+{
+    public class MyVector2Int
+    {
+        public int x, y;
+
+        public MyVector2Int(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
---
+++ Assets/Copypasta/Vector/Int/MyVector3Int.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Copypasta
+{
+    public struct MyVector3Int
+    {
+        public int x, y, z;
+
+        public MyVector3Int(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
