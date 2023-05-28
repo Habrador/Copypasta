67bb90a0eee85415ba318ebe93e7bfb757d123e6
habrador <erik.nordeus@gmail.com>
2023-05-28 23:28:06 +0200

    Added test scene to test the vector library

---
+++ Assets/Scenes/TestVectors.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Copypasta;
+
+
+public class TestVectors : MonoBehaviour
+{
+    private Vector3 vector3;
+    private MyVector3 myVector3;
+
+    private void Start()
+    {
+        TestVector3(vector3);
+
+        //TestMyVector3(myVector3); //Error!!!
+    }
+
+    private void TestVector3(Vector2 vector2)
+    {
+        
+    }
+
+    private void TestMyVector3(MyVector2 vector2)
+    {
+
+    }
+}
